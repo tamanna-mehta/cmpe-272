e66c9660f1ff327fa1af2df538a3f0a66ddb7348
bharathGuna <bharathguna610@gmail.com>
2020-10-04 20:02:49 -0700

    getting all tweets and username

---
+++ assignment-2/middleware/get-all-tweets.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const request = require('../lib/http');
+
+const verifyCredentials = async (req, res) => {
+    // Checking if required param exist
+    let response = {};
+    try {
+        // Making Twitter Request
+        response = await request({
+            method: 'GET',
+            uri: `https://api.twitter.com/1.1/statuses/user_timeline.json`,
+            resolveWithFullResponse: true,
+            oauth: req.auth,
+
+        })
+        response.body = JSON.parse(response.body);
+    }
+    catch (err) {
+        response.body = JSON.parse(err.response.body);
+    };
+
+    // Sending response to client
+    res.json(response.body);
+};
+
+module.exports = verifyCredentials;
--- assignment-2/routes/external/twitter.js
+++ assignment-2/routes/external/twitter.js
@@ -3,12 +3,10 @@ const express = require('express');
 const postTweet = require('../../middleware/post-tweet');
 const deleteTweet = require('../../middleware/delete-tweet');
 const getTweet = require('../../middleware/get-tweet');
+const verifyCredentials = require('../../middleware/verify-credentials');
+const getAllTweets = require('../../middleware/get-all-tweets');
 
 const twitter = express.Router();
-
-twitter.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 // Routes for creating, getting, and deleting a tweet
 twitter.post('/update', postTweet);
@@ -16,5 +14,9 @@ twitter.post('/update', postTweet);
 twitter.post('/delete/:id', deleteTweet);
 
 twitter.get('/:id', getTweet);
+
+twitter.get('/', verifyCredentials);
+
+twitter.get('/all/:id', getAllTweets);
 
 module.exports = twitter;
